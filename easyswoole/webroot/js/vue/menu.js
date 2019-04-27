e939435ea76f8309a4fba36e5884f4420d907622
ming <1023125136@qq.com>
2019-04-27 20:07:05 +0800

    js vue

---
+++ easyswoole/webroot/js/vue/menu.js
@@ -0,0 +1,44 @@
+var menu = new Vue({
+    el: '#menu',
+    data: {
+        video: [
+            { title: '三生三世' },
+            { title: '倚天屠龙记' },
+            { title: '致青春' },
+            { title: '白娘子传奇' },
+        ],
+        category: {
+            one: [
+                { title: '体育' },
+                { title: '游戏' },
+                { title: '音乐' },
+                { title: '资讯' },
+                { title: '搞笑' },
+            ],
+            two: [
+                { title: '综艺' },
+                { title: '儿童' },
+                { title: '动漫' },
+                { title: '漫画' },
+                { title: '文学' },
+            ],
+            three: [
+                { title: '知识' },
+                { title: '电影' },
+                { title: '热点' },
+                { title: '历史' },
+                { title: '电影' },
+            ],
+            four: [
+                { title: '知识' },
+                { title: '电影' },
+                { title: '热点' },
+                { title: '历史' },
+                { title: '电影' },
+            ],
+        },
+    },
+    methods: {
+
+    },
+})
---
+++ easyswoole/webroot/js/vue/top.js
@@ -0,0 +1,10 @@
+var top = new Vue({
+    el: '#top',
+    data: {
+        message: 'Welcome to KOOLTUBE!',
+    },
+    methods: {
+
+    },
+
+})
